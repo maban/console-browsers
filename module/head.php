fee61b0d826fbfca2a87f0b41ab03411736298e1
Anna Debenham <anna@maban.co.uk>
2012-10-06 15:20:28 +0100

    Changing base url

--- module/head.php
+++ module/head.php
@@ -5,7 +5,7 @@
 	<title>Game Console Browsers</title>	
 	<meta name="description" content="Anna Debenham, freelance front-end developer based in Brighton" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<base href="<?php echo "http://".$_SERVER['HTTP_HOST']; ?>/console-browsers/" />
+	<base href="<?php echo "http://".$_SERVER['HTTP_HOST']; ?>" />
 	<link rel="stylesheet" type="text/css" media="screen" href="style/base.css?201210052354" />
 	<link rel="stylesheet" type="text/css" media="print" href="style/print.css?201210052354" />
 	<link rel="stylesheet" type="text/css" media="screen and (min-width:800px)" href="style/layout.css?201210052354" />
