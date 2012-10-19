8ed45bcf11a0071a28111f60400fe244ba858334
Anna Debenham <anna@maban.co.uk>
2012-10-19 12:17:11 +0100

    Added notes on the PSP-1000

---
+++ device/psp-1000.php
@@ -0,0 +1,109 @@
+<?php include('../module/head.php');?>
+<body class="section-device">
+<header id="top">
+	<?php include('../module/nav.php');?>
+	<h1 class="heading">Xbox 360</h1>
+</header>
+
+<section class="content">
+	
+<article>
+		
+	<div class="entry-content">
+		
+		<h2>About the PSP-1000</h2>
+		
+		<aside><p><a href="http://en.wikipedia.org/wiki/PlayStation_Portable">More about the PSP-1000 on Wikipedia</a></p></aside>
+		
+		<p>The screen is not a touch screen. There is a small cursor that can be moved around using the left thumbstick. The d-pad cycles through links on the page and scrolls.</p>
+		
+		<p>The left and right shoulder pads move back and forward through the browser history. Holding down the square button and tapping the right shoulder pad opens up a new tab. There can be up to 3 tabs open at once.</p>
+		
+		<p>The browser standard is very low. A lot of sites don't load, or content doesn't appear. Typing is very fiddly, keyboard is SMS style and the select and cancel keys are mapped differently to modern PlayStation controls (select and cancel are swapped round)</p>
+		
+		<p>Text size can be adjusted in the settings.</p>
+		
+		
+		
+		<h3>Media query tests</h3>
+		
+		<aside><p>Source: <a href="http://www.jordanm.co.uk/palmreader">Palm Reader</a></p></aside>
+		
+		<h4>What the device can read:</h4>
+		
+		<ul>
+			<li><code>screen</code></li>
+			
+		</ul>
+		
+		<h4>What the device says it isn't:</h4>
+		
+		<ul>
+			<li><code>print</code></li>
+			<li><code>tv</code></li>
+			<li><code>color</code></li>
+			<li><code>monochrome</code></li>
+			<li><code>handheld</code></li>
+			<li><code>projection</code></li>
+			<li><code>(orientation : portrait)</code></li>
+			<li><code>(orientation : landscape)</code></li>
+			<li><code>(min-width)</code></li>
+			<li><code>(max-width)</code></li>
+			<li><code>(-webkit-min-device-pixel-ratio: 2)</code></li>
+			<li><code>(pointer:coarse)</code></li>
+			<li><code>(pointer:fine)</code></li>
+			<li><code>(pointer:none)</code></li>
+		</ul>	
+		
+		<h2>Controls</h2>
+		
+		<figure>
+			<a href="../image/diagram-psp-1000-controller.png">
+				<img src="../image/diagram-psp-1000-controller.png" alt="diagram of the PS Vita" />
+				<figcaption>
+					<p>See big version</p>
+				</figcaption>
+			</a>
+		</figure>
+		
+	</div><!-- .entry-content -->
+	
+	<div class="supplementary">
+	
+		<img src="../image/psp1000-controller.png" alt="Photo of the PSP 1000 controller." />
+		
+		<h2>Browser</h2>
+		
+		<p>Unknown. Reports itself as Mozilla.</p>
+		
+		<h2>Support Details</h2>
+		
+		<table>
+			<tr>
+				<th>User Agent</th>
+				<td><code>Mozilla/4.0 (PlayStation Portable); 2.00)</code></td>
+			</tr>
+		</table>
+		
+		<div class="highlight highlight-html">
+			<h3>HTML5 Support</h3>
+			<p>Test wouldn't run</p>
+			<p>as of October 2012</p>
+			<aside><p>Source: <a href="http://html5test.com">html5test.com</a></p></aside>
+		</div>
+		
+		<div class="highlight highlight-css">
+			<h3>CSS3 Support</h3>
+			<p>Test wouldn't run</p>
+			<p>as of October 2012</p>
+			<aside><p>Source: <a href="http://css3test.com">css3test.com</a></p></aside>
+		</div>
+		
+	</div><!-- .supplementary -->
+	
+</article>
+
+</section>
+<?php include('../module/foot.php');?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php include('module/head.php');?>
+
 <body class="section-home">
+
 <header id="top">
 	<?php include('module/nav.php');?>
 	<h1 class="heading">Game console browsers</h1>
@@ -8,15 +10,29 @@
 <section class="content">
 
 <div class="entry-content">
+	
 	<h2>Tested devices:</h2>
+	
 	<p>I've written up some of the tests I ran on these devices, and they're now part of <a href="http://clearleft.com/testlab/">Clearleft's community test lab</a>. (Except for the Wii, which was borrowed, and the PS3, which lives with me.)</p>
+	
 	<nav class="nav-primary">
+		
+		<h3>TV consoles:</h3>
+		
 		<ul>
 			<li class="device">
 				<article id="device-ps3">
 					<a class="description" href="device/ps3.php">
 						<h1 class="title">Sony PS3</h1>
 						<img src="image/ps3-controller.png" alt="Picture of the PS3" />
+					</a>
+				</article>
+			</li>
+			<li class="device">
+				<article id="device-wii">
+					<a class="description" href="device/wii.php">
+						<h1 class="title">Wii</h1>
+						<img src="image/wii-controller.png" alt="Picture of the Wii controller" />
 					</a>
 				</article>
 			</li>
@@ -28,6 +44,13 @@
 					</a>
 				</article>
 			</li>
+		</ul>
+		
+		<h3>Portable consoles:</h3>
+		
+		<h4>Sony PlayStation Portable line:</h4>
+		
+		<ul>		
 			<li class="device">
 				<article id="device-xbox">
 					<a class="description" href="device/psvita.php">
@@ -37,13 +60,18 @@
 				</article>
 			</li>
 			<li class="device">
-				<article id="device-wii">
-					<a class="description" href="device/dsi.php">
-						<h1 class="title">Nintendo DSi</h1>
-						<img src="image/dsi-controller.png" alt="Picture of the DSi" />
+				<article id="device-psp-1000">
+					<a class="description" href="device/psp-1000.php">
+						<h1 class="title">PSP-1000</h1>
+						<img src="image/psp-1000-controller.png" alt="Picture of the PSP-1000" />
 					</a>
 				</article>
 			</li>
+		</ul>
+			
+		<h4>Nintendo DS line:</h4>
+		
+		<ul>
 			<li class="device">
 				<article id="device-ps3">
 					<a class="description" href="device/3dsxl.php">
@@ -51,7 +79,20 @@
 						<img src="image/3dsxl-controller.png" alt="Picture of the 3DS XL" />
 					</a>
 				</article>
+			</li>			
+			<li class="device">
+				<article id="device-wii">
+					<a class="description" href="device/dsi.php">
+						<h1 class="title">Nintendo DSi</h1>
+						<img src="image/dsi-controller.png" alt="Picture of the DSi" />
+					</a>
+				</article>
 			</li>
+		</ul>
+		
+		<h3>Hybrid Phone/Portable console</h3>
+		
+		<ul>
 			<li class="device">
 				<article id="device-xperia">
 					<a class="description" href="device/xperiaplay.php">
@@ -60,24 +101,31 @@
 					</a>
 				</article>
 			</li>
-			
 		</ul>
+		
 	</nav>
-	</div>
+	
+</div>
+
 </section>
+
 <section class="nav-secondary">
+	
 	<div class="links">
+		
 		<h2>Not tested:</h2>
+		
 		<p>These are devices that have browsers but I don't have them in my library yet.</p>
+		
 		<ul>
-			<li>DS</li>
+			<li>DS (notes on this one coming soon)</li>
 			<li>DSi XL</li>
 			<li>3DS</li>
-			<li>PSP 1000</li>
 			<li>PSP 2000</li>
 			<li>PSP 3000</li>
 			<li>PSP Go (PSP N1000)</li>
 		</ul>
+	
 	</div>
 	
 	<div class="links">	
@@ -89,9 +137,11 @@
 			<li>Razer Switchblade</li>
 		</ul>
 	</div>
+	
 </section>	
 
 <section class="nav-tertiary">
+	
 	<div class="links reports">	
 		<h2>Reports</h2>
 		<ul>
@@ -101,6 +151,7 @@
 			</li>
 		</ul>
 	</div>
+	
 	<div class="links">
 		<h2>Useful Links</h2>
 		<ul>
@@ -110,6 +161,7 @@
 			</li>
 		</ul>
 	</div>
+	
 </section>
 
 <?php include('module/foot.php');?>
